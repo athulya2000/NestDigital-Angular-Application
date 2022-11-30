a2b9af51642c3980fa3f66c5454835f6ccfe7856
athulya2000 <athulyav00@gmail.com>
2022-11-30 13:39:37 +0530

    career

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CareersComponent } from './careers/careers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CareersComponent
   ],
   imports: [
     BrowserModule,
